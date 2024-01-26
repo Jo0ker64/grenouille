7ca38fa29c591e62a6f67d9462779d78ff0f5df0
jaune-attend64 <martel.jonathan64@gmail.com>
2024-01-26 14:39:40 +0100

    maj

--- scripts/Game.js
+++ scripts/Game.js
@@ -1,184 +1,169 @@
-function LaunchGame() { // fonction pour lancer le jeux avec toutes les variables et autres commandes
+function LaunchGame() {
+    let life_count = 3;
+    let time = 60;
+    let score = 0;
+    let wl_full = 0;
+    let loading = 0;
 
-    // déclarer les variables exemple temps, vie, .....
+    let canvas = document.getElementById("board");
 
-    let life_count = 3; // 3 vies avant de mourir
-    let time = 60; //60 secondes de jeux
-    let score = 0;  // score demarre à zero
-    let wl_full = 0;  // 
-    let loading = 0;  //load le jeu au début
+    if(canvas.getContext){
+        ctx = canvas.getContext("2d");
+        setInterval(main_loop, 16);
+        window.addEventListener("keydown", whatKey, true);
+        loading = loading + 5; 
+    }
 
-    let canvas = document.getElementById("board");   // 
+    let frog_sound = new Audio("sounds/frog.wav");
+    let water_sound = new Audio("sounds/water.wav");
+    let hit_sound = new Audio("sounds/hit.wav");
+    let boing_sound = new Audio("sounds/boing.wav");
+    let bell_sound = new Audio("sounds/bell.wav");
+    frog_sound.oncanplaythrough = loading = loading + 3;
+    water_sound.oncanplaythrough = loading = loading + 3;
+    hit_sound.oncanplaythrough = loading = loading + 3;
+    boing_sound.oncanplaythrough = loading = loading + 3;
+    bell_sound.oncanplaythrough = loading = loading + 3;
 
-    if (canvas.getContext) {
-
-        ctx = canvas.getContext('2d');
-        setInterval(main_loop, 16);     //mettre la boucle du jeu, l'affiche de la vie, l'affichage de tout 
-        window.addEventListener('keydown', whatKey, true);  //fonction de clavier
-        loading = loading + 5;
-
-    };
-
-    let frog_sound = new Audio("sounds/frog.waw");  // son grenouille
-    let water_sound = new Audio("sound/water.waw"); // son eau
-    let hit_sound = new Audio("sound/hit.waw");  // son gagné
-    let boing_sound = new Audio("sound/boing.waw"); // son saut
-    let bell_sound = new Audio("sound/bell.waw");  // son cloche
-    frog_sound.oncanplaythrough = loading = loading + 3;  // load du son
-    water_sound.oncanplaythrough = loading = loading + 3;  // load du son
-    hit_sound.oncanplaythrough = loading = loading + 3;  // load du son
-    boing_sound.oncanplaythrough = loading = loading + 3; // load du son
-    bell_sound.oncanplaythrough = loading = loading + 3;    //load du son 
-
-    let imgs = new Array(21); // charger toutes les images en 1 fois
-
-    for (i = 0; i < imgs.length; i++) {
+    let imgs = new Array (21);
+    for(i=0 ; i<imgs.length ; i++){
         imgs[i] = new Image();
-        imgs[i].onload = function () {
+        imgs[i].onload = function(){
             loading = loading + 3;
+
         };
-        imgs[i].src = "imgs/img_" + i + ".gif"; // charge toutes les images avec une ligne au lieu de faire une ligne par image
-
-    };
-
-    function setTimer() {
+        imgs[i].src = "imgs/img_"+i+".gif";
+    }
+    function setTimer(){
         time = 60;
-        interval = setInterval(function () {
-            if (time == 0) {
+        interval = setInterval(function() {
+            if(time==0){
                 playSound(bell_sound);
                 loseLife();
-            } // pas de ; quand c'est un if
+            }
         }, 1000);
     };
-
-    function playSound() {
-        // lance le son
-    };
-
-    function loseLife() {
-        // perdu
+   
+    function playSound(){
 
     };
 
-    function writeMessage() {
-        // message 
+    function loseLife(){
 
     };
 
-    //voiture
-    let line1 = new Line(Math.floor((Math.random() * 3) + 1), -1, 34, 1);
-    let line2 = new Line(Math.floor((Math.random() * 3) + 1), 1, 28, 2);
-    let line3 = new Line(Math.floor((Math.random() * 3) + 1), -1, 33, 3);
-    let line4 = new Line(Math.floor((Math.random() * 3) + 1), 1, 34, 4);
-    let line5 = new Line(Math.floor((Math.random() * 3) + 1), -1, 54, 5);
-    // calcul laffichage de la vitesse, du déplacement, la taile et le nombre de voiture *3 +1 speed, -1 direction, 34 taille de voiture, 1 c'est la position et le type de la voiture
-
-    //buche
-    let line6 = new Line(Math.floor((Math.random() * 3) + 1), 1, 117, 6);
-    let line7 = new Line(Math.floor((Math.random() * 3) + 1), -1, 180, 7);
-    let line8 = new Line(Math.floor((Math.random() * 3) + 1), -1, 84, 8);
-    let line9 = new Line(Math.floor((Math.random() * 3) + 1), -1, 180, 7);
-    let line10 = new Line(Math.floor((Math.random() * 3) + 1), -1, 117, 6);
-
-    let enms = new Array;
-    enms[0] = new Obj(50, 350, line1);
-    enms[1] = new Obj(250, 350, line1);
-    enms[2] = new Obj(450, 350, line1);
-    enms[3] = new Obj(120, 320, line2);
-    enms[4] = new Obj(250, 320, line2);
-    enms[5] = new Obj(380, 320, line2);
-    enms[6] = new Obj(120, 290, line3);
-    enms[7] = new Obj(250, 290, line3);
-    enms[8] = new Obj(380, 290, line3);
-    enms[9] = new Obj(120, 260, line4);
-    enms[10] = new Obj(250, 260, line4);
-    enms[11] = new Obj(380, 260, line4);
-    enms[12] = new Obj(120, 230, line5);
-    enms[13] = new Obj(250, 230, line5);
-
-    let plts = new Array;
-    plts[0] = new Obj(50, 170, line6);
-    plts[1] = new Obj(450, 170, line6);
-    plts[2] = new Obj(850, 170, line6);
-    plts[3] = new Obj(120, 140, line7);
-    plts[4] = new Obj(450, 140, line7);
-    plts[5] = new Obj(750, 170, line7);
-    plts[6] = new Obj(320, 110, line8);
-    plts[7] = new Obj(720, 110, line8);
-    plts[8] = new Obj(900, 110, line8);
-    plts[9] = new Obj(120, 80, line9);
-    plts[10] = new Obj(420, 80, line9);
-    plts[11] = new Obj(720, 80, line9);
-    plts[12] = new Obj(120, 50, line10);
-    plts[13] = new Obj(420, 50, line10);
-    plts[14] = new Obj(720, 50, line10);
-
-    let wls = new Array;
-    wls[0] = new WaterLilly(42, 0);
-    wls[1] = new WaterLilly(162, 0);
-    wls[2] = new WaterLilly(282, 0);
-    wls[3] = new WaterLilly(482, 0);
-    wls[4] = new WaterLilly(522, 0);
-
-    let frog = new Frog();
-    loading = loading + 17;
-
-
-
-    function main_loop() {
-        //initialiser les touches du claviers fleches et entrer
+    function writeMessage(){
 
     };
 
-    function whatKey(evt) {
-        // défini les touches du clavier
-        switch (evt.keyCode) {
+let line1 = new Line(Math.floor((Math.random()*3) + 1), -1, 34, 1);
+let line2 = new Line(Math.floor((Math.random()*3) + 1), 1, 28, 2);
+let line3 = new Line(Math.floor((Math.random()*3) + 1), -1, 33, 3);
+let line4 = new Line(Math.floor((Math.random()*3) + 1), 1, 34, 4);
+let line5 = new Line(Math.floor((Math.random()*3) + 1), -1, 54, 5);
+
+let line6 = new Line(Math.floor((Math.random()*3) + 1), 1, 117, 6);
+let line7 = new Line(Math.floor((Math.random()*3) + 1), -1, 180, 7);
+let line8 = new Line(Math.floor((Math.random()*3) + 1), -1, 84, 8);
+let line9 = new Line(Math.floor((Math.random()*3) + 1), -1, 180, 7);
+let line10 = new Line(Math.floor((Math.random()*3) + 1), -1, 117, 8);
+
+let enms = new Array;
+enms[0] = new Obj(50 ,350 ,line1) ;
+enms[1] = new Obj(250, 350, line1);
+enms[2] = new Obj(450, 350, line1);
+enms[3] = new Obj(120, 320, line2);
+enms[4] = new Obj(250, 320, line2);
+enms[5] = new Obj(380, 320, line2);
+enms[6] = new Obj(120, 290, line3);
+enms[7] = new Obj(250, 290, line3);
+enms[8] = new Obj(380, 290, line3);
+enms[9] = new Obj(120, 260, line4);
+enms[10] = new Obj(250, 260, line4);
+enms[11] = new Obj(380, 260, line4);
+enms[12] = new Obj(120, 230, line5);
+enms[13] = new Obj(250, 230, line5);
+
+let plts = new Array;
+plts[0] = new Obj(50, 170, line6);
+plts[1] = new Obj(450, 170, line6);
+plts[2] = new Obj(850, 170, line6);
+plts[3] = new Obj(120, 140, line7);
+plts[4] = new Obj(450, 140, line7);
+plts[5] = new Obj(790, 140, line7);
+plts[6] = new Obj(320, 110, line8);
+plts[7] = new Obj(620, 110, line8);
+plts[8] = new Obj(920, 110, line8);
+plts[9] = new Obj(120, 80, line9);
+plts[10] = new Obj(420, 80, line9);
+plts[11] = new Obj(720, 80, line9);
+plts[12] = new Obj(120, 50, line10);
+plts[13] = new Obj(320, 50, line10);
+plts[14] = new Obj(520, 50, line10);
+
+let wls = new Array;
+wls[0] = new WaterLily(42, 0);
+wls[1] = new WaterLily(162, 0);
+wls[2] = new WaterLily(282, 0);
+wls[3] = new WaterLily(402, 0);
+wls[4] = new WaterLily(522, 0);
+
+let frog = new Frog();
+loading = loading + 17;
+
+
+function main_loop() {
+        
+    };
+
+    function whatKey(evt){
+        switch(evt.keyCode){
             case 37:
                 frog.goLeft();
                 playSound(boing_sound);
                 anim_time = 8;
-                break;
+            break;
             case 39:
                 frog.goRight();
                 playSound(boing_sound);
                 anim_time = 8;
-                break;
+            break;
             case 40:
                 frog.goDown();
                 playSound(boing_sound);
                 anim_time = 8;
-                break;
+            break;
             case 38:
                 frog.goUp();
                 playSound(boing_sound);
                 anim_time = 8;
-                if (frog.y < last_y && frog.state == true) {
+                if(frog.y < last_y && frog.state == true){
                     last_y = frog.y;
                     score = score + 50;
                 }
-                break;
+            break;
             case 82:
-                if (message_state == 2 || message_state == 3) {
+                if(message_state == 2 || message_state == 3){
                     life_count = 3;
                     score = 0;
                     last_y = 380;
                     wl_full = 0;
-                    for (i = 0; i < wls.length; i++) {
+                    for(i = 0; i < wls.length; i++ ){
                         wls[i].setUnused();
                     }
                     message_state = 0;
                     frog.initPos();
-                    setTimer();
+                    setTimer(); 
                 }
             break;
             case 13:
-                if (loading == 100){
+                if(loading == 100){
                     loading = 0;
                     frog.unBlock();
                     begin = true;
                     setTimer();
-                }
-            break;       
         }
-    }
+            break;
+    };
+}
 }
--- scripts/class.js
+++ scripts/class.js
@@ -1,43 +1,79 @@
-function Line(speed, dir, width, type){
+function Line(speed, dir, width, type) {
     this.speed = speed;
     this.dir = dir;
     this.width = width;
-    this.type = type;
+    this.type = type;    
+}
 
-};
-
-function Obj( x, y, line){
+function Obj(x, y, line){
     this.x = x;
     this.y = y;
     this.line = line;
 
-    this.inCollision = function(frog){
-        if(frog.x + 25 >= this.x && frog.x+5 <=(this.x + this.line.width)){
+    this.InCollision = function(frog){
+        if(frog.x + 25 >= this.x && frog.x + 5 <= (this.x + this.line.width)){
             return true;
-        }
-
-        else{
-            return false;
-        }
-    }; 
-
+        }else {return false}
+    }
+    
     this.move = function(){
         if(this.line.dir == 1){
-            if (this.x > 650){
-                this.x = - (this.line.width * 3);
+            if(this.x > 650){
+                this.x = -(this.line.width*3);
             }
             else{
-                this.x = this.x + (this.line.speed * this.line.dir);
+                this.x = this.x +(this.line.speed*this.line.dir);
             }
         }
         else{
-            if( this.x < - this.line.width){
-                this.x = 650 + ( this.line.width * 3);
-            }
-            else{
-                this.x = this.x + (this.line.speed * this.line.dir);
+            if(this.x < -this.line.width){
+                this.x = 650 +(this.line.width*3);
+            }else{
+                this.x = this.x  +(this.line.speed*this.line.dir);
             }
         }
-    };
+    }
+}
 
+function Frog(){
+    this.x = 310;
+    this.y = 380;
+    this.state = false;
+    this.deg = 0;
+    this.block = function(){
+        this.state = false;
+    };
+    this.unBlock = function(){
+        this.state = true;
+    };
+    this.isNotBlocked = function(){
+        return this.state;
+    };
+    this.goUp = function(){
+        if(this.state == true){
+            if(this.y > 20){
+                this.y = this.y - 30; 
+            }
+            this.deg = 0;
+        }
+    }
+    this.goDown = function(){
+        if(this.state = true){
+            if(this.y = 170){
+                if(this.x > 310){
+                     while(((this.x - 10)%30)!=0){
+                        this.x--;
+                     }
+                }else{
+                    while(((this.x -10)%30)!=0){
+                        this.x++;
+                    }
+                }
+            }
+            if(this.y < 380){
+                this.y = this.y+30;
+            }
+            this.deg=180;
+        }
+    }
 };
